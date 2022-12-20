aefd093d7bec6cbb272c053e8e04ab427a900fa1
marc1k3y <marc1k3y@gmail.com>
2022-12-20 23:20:54 +0300

    repair outTableActions

--- src/components/outTableActions/index.tsx
+++ src/components/outTableActions/index.tsx
@@ -7,21 +7,19 @@ interface IAction {
 	element: JSX.Element
 }
 
+const library: IAction[] = [
+	{ access: ["3", "4", "7"], element: <CreateAccountRequestButton /> }
+]
+const filteredLibrary = library.filter(action => action.access.includes(roleId))
+
 export const OutTableActions = () => {
 	const [result, setResult] = useState<IAction[] | null>(null)
 
-	const library: IAction[] = [
-		{ access: ["1", "3", "4", "7"], element: <CreateAccountRequestButton /> }
-	]
-
 	useEffect(() => {
-		// @ts-ignore
-		const filteredLibrary = library.filter(action => action.access.includes(roleId))
 		setResult(filteredLibrary)
 	}, [])
 	if (result) return (
 		<div>
-			{/* @ts-ignore */}
 			{result.map((item, i) => (
 				<React.Fragment key={i}>
 					{item.element}
