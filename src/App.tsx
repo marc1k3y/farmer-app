8be7245aeeea4a3b926583dae479d9ffe176c03e
Marck <76914057+marc1k3y@users.noreply.github.com>
2022-12-20 13:52:08 +0300

    mob test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   // @ts-ignore
   const { isAuth } = useSelector(state => state.auth)
   console.log(isAuth);
-  
+   console.log("hello from mobile")
   return (
     <div className="App">
       {isAuth ? <MainPage /> : <AuthPage />}
